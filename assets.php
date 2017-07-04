162a766c30fab17e379af8695d378e7302b8b1b4
Meathill <meathill@gmail.com>
2017-07-04 20:21:09 +0800

    修复生成路径的问题
    Signed-off-by: Meathill <meathill@gmail.com>

--- assets.php
+++ assets.php
@@ -1,11 +1,11 @@
 <?php
 
 function resize_by_qiniu( $html, $post_id, $post_thumbnail_id, $size ) {
-  if( $html && !$size ) {
+  if($html) {
     $html = preg_replace_callback('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', function ($matches) {
       $img = $matches[1];
-      if (preg_match('//qiniu.meathill.com/', $img)) {
-        $img .= '?imageView2/0/w/1200';
+      if (preg_match('~//qiniu.meathill.com/~', $img)) {
+        $img = $img . '?imageView2/0/w/1200';
       }
       return str_replace($matches[1], $img, $matches[0]);
     }, $html);
