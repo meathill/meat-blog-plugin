4dca52da02bc8002fb628afe507bd5dbcbe7fd0b
Meathill <meathill@gmail.com>
2017-07-15 16:54:45 +0800

    貌似事件的位置不对
    Signed-off-by: Meathill <meathill@gmail.com>

--- content.php
+++ content.php
@@ -6,16 +6,21 @@
  * Time: 15:43
  */
 
-function add_weixin_qrcode($content) {
-  $content .= '<div class="weixin-gathering">
+function check_weixin() {
+  /**
+   * @param string $content
+   * @return string
+   */
+  function add_weixin_qrcode($content) {
+    $content .= '<div class="weixin-gathering">
   <hr>
   <p>随手打赏，满身余香。您的支持将鼓励我继续创作！</p>
   <p><img src="http://qiniu.meathill.com/image/weixin-gathering.png"></p>
 </div>';
-  return $content;
+    return $content;
+  }
+  if (is_single() && preg_match('/\bmicromessenger\b/', $_SERVER['HTTP_USER_AGENT'])) {
+    add_filter('the_content', 'add_weixin_qrcode', 10);
+  }
 }
-
-if (is_single() && preg_match('/\bmicromessenger\b/', $_SERVER['HTTP_USER_AGENT'])) {
-  add_filter('the_content', 'add_weixin_qrcode', 5);
-}
-
+add_action('template_redirect', 'check_weixin');
