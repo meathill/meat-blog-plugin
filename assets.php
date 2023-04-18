b15aada93e95ef5e13c7287447e741ef0453f210
Meathill Zhai <meathill@gmail.com>
2023-04-18 03:20:35 +0000

    * chore: update jQuery * deps: update qrcode * change: disabled SQL_CALC_FOUND_ROWS to fix pagination

--- assets.php
+++ assets.php
@@ -4,6 +4,9 @@ function resize_by_qiniu( $html, $post_id, $post_thumbnail_id, $size ) {
   if($html) {
     $html = preg_replace_callback('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', function ($matches) {
       $img = $matches[1] . '?imageView2/0/w/1200';
+#echo  '<!--';
+#var_dump(parse_url($img),parse_url($matches[1]));
+#echo '-->';
       return str_replace($matches[1], $img, $matches[0]);
     }, $html);
   }
